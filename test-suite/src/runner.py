52c329be302f77071b5e9fa723ed144f1080c25a
Matteo Grandini <matteo.grandini@studio.unibo.it>
2026-09-03 16:50:37 +0200

    Fixed runner timestamp bug. Added tests/all.json

--- test-suite/src/runner.py
+++ test-suite/src/runner.py
@@ -26,7 +26,8 @@ class TestRunner:
             short_container_id = container.id[:12]
             logger.info(f"[{test.id}] Created container with ID: {short_container_id}")
             container.start()
-            executed_at = datetime.now(tz=timezone.utc)
+            container.reload()
+            executed_at = self._docker_time_to_datetime(container.attrs['State']['StartedAt'])
             logger.info(f"[{test.id}] Started container. Running test...")
             container.wait(timeout=60)
             container.remove(force=True)
@@ -36,3 +37,12 @@ class TestRunner:
 
         duration_ms = (time.time() - start_time) * 1000
         return short_container_id, executed_at, duration_ms
+
+    def _docker_time_to_datetime(self, docker_time_str: str) -> datetime:
+        try:
+            cut_nanoseconds = docker_time_str[:-4]
+            dt = datetime.strptime(cut_nanoseconds, "%Y-%m-%dT%H:%M:%S.%f")
+            return dt.replace(tzinfo=timezone.utc)
+        except ValueError as e:
+            logger.error(f"Error parsing Docker time string '{docker_time_str}': {e}")
+            return None
